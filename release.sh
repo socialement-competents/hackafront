a2d9c4c9e0236eab0e01c99875b9efcae6f11de0
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-05 01:22:30 +0200

    Use a debian image because Circle CI doesn't recognize '@machin.zip' nor '-z'

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "x${GITHUB_API_TOKEN}" = "x" ]; then
+if [ -z "${GITHUB_API_TOKEN}" ]; then
   echo "Please add the GITHUB_API_TOKEN env var"
   exit 1
 fi
 
-if [ "x${CIRCLE_TAG}" = "x" || "x${CIRCLE_BRANCH}" = "x" ]; then
+if [ -z "${CIRCLE_TAG}" || -z "${CIRCLE_BRANCH}" ]; then
   echo "Run this script in Circle CI (or setup CIRCLE_TAG and CIRCLE_BRANCH manually to test)"
   exit 1
 fi
