41c999a2487a88485415bca6834214aab2a883d2
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-05 03:12:37 +0200

    Clean the zip

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Add all files and folders, recursively, to a tag-named .zip 
+FILENAME="$CIRCLE_PROJECT_REPONAME-$CIRCLE_TAG.zip"
+zip -r $FILENAME *
+
+# Download the github-release executable
 GITHUB_RELEASE_GO=https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2
 curl -sL $GITHUB_RELEASE_GO | tar xvjf -
 GR=bin/linux/amd64/github-release
@@ -11,10 +16,6 @@ $GR release \
   --repo $CIRCLE_PROJECT_REPONAME \
   --tag $CIRCLE_TAG \
   --name $CIRCLE_TAG
-
-# Add all files and folders, recursively, to a tag-named .zip 
-FILENAME="$CIRCLE_PROJECT_REPONAME-$CIRCLE_TAG.zip"
-zip -r $FILENAME *
 
 # Upload the .zip to the release as an asset
 $GR upload \
